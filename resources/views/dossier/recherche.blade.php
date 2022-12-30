2b35d9993ce0046575cca32bed6ea969c609ff8c
mehdiwac1994 <61623280+mehdiwac1994@users.noreply.github.com>
2022-12-30 16:04:07 +0100

    PUSH

--- resources/views/dossier/recherche.blade.php
+++ resources/views/dossier/recherche.blade.php
@@ -335,5 +335,14 @@ console.log(data);
       <script src="{{asset('assets/js/dossier_recherche_table.js')}}"></script>
 
 
+      <script src="https://cdn.datatables.net/responsive/2.4.0/js/responsive.bootstrap.min.js"></script>
+      <script src="https://cdn.datatables.net/responsive/2.4.0/js/dataTables.responsive.min.js"></script>
+      <script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap.min.js"></script>
+      <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+      <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+      <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+     
+
+
    
 @endsection
